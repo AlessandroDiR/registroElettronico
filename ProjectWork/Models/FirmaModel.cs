efd2a37c1e58dc305a09f47a715ba940239f91aa
themustang <diromaalessandro@gmail.com>
2020-03-02 21:54:04 +0100

    firma da get a post backend

---
+++ ProjectWork/Models/FirmaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWork.Models
+{
+    public class FirmaModel
+    {
+        public string code { get; set; }
+        public int idCorso { get; set; }
+        public int anno { get; set; }
+    }
+}
--- ProjectWork/wwwroot/service-worker.js
+++ ProjectWork/wwwroot/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b11348d97714fa398827b56c8831843.js"
+  "/precache-manifest.90626f7b2015b8ebd2e8401fecb76900.js"
 );
 
 self.addEventListener('message', (event) => {
