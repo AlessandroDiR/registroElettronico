6c4017ad2768860dbf4b66e9fd683b6639cfd3e8
themustang <diromaalessandro@gmail.com>
2020-03-18 20:41:20 +0100

    merge

--- ProjectWork/Models/AvocadoDBContext.cs
+++ ProjectWork/Models/AvocadoDBContext.cs
@@ -36,7 +36,7 @@ namespace ProjectWork.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-DKF8A9U;Database=AvocadoDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=dell-alessandro\\dell_alessandro;Database=AvocadoDB;Trusted_Connection=True;");
             }
         }
 
