44659557cacf7a1bcf4b856f455326619091348c
leio99 <amicoexe@gmail.com>
2020-03-26 15:13:03 +0100

    Config calendario con password

--- FrontEnd/src/comp/admin/ConfigForm.tsx
+++ FrontEnd/src/comp/admin/ConfigForm.tsx
@@ -4,6 +4,7 @@ import Axios from "axios"
 import { siteUrl } from "../../utilities"
 import { ICalendar } from "../../models/ICalendar"
 import Calendario from "../Calendario"
+import { askPassword } from "../AskConferma"
 
 export interface IProps{
     readonly anno: number
@@ -62,22 +63,24 @@ export default class ConfigForm extends React.PureComponent<IProps, IState>{
 
             return
         }
-
-        this.setState({
-            actualId: ""
-        })
-
-        Axios.post(siteUrl+"/api/calendari", {
-            IdCalendario: calendar ? calendar.idCalendario : "0",
-            IdCorso: corso,
-            Anno: anno,
-            IdGoogleCalendar: calendarId
-        }).then(_ => {
+        
+        askPassword(siteUrl+"/api/calendari", "post", {
+            calendario: {
+                idCalendario: calendar ? calendar.idCalendario : "0",
+                idCorso: corso,
+                anno: anno,
+                idGoogleCalendar: calendarId
+            }
+        }, (_: any) => {
             this.setState({
                 actualId: calendarId
             })
 
             message.success("Configurazione calendario salvata!")
+        }, () => {
+            this.setState({
+                actualId: ""
+            })
         })
     }
 
