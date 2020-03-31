4a5cb0874f11de38183b182ccf2b0f6ad76a4ad7
themustang <diromaalessandro@gmail.com>
2020-03-31 16:06:16 +0200

    Frontend updated

---
+++ ProjectWork/wwwroot/precache-manifest.a773c65b093a9fd7032aa788f46246c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf72df3d579e23f492faa240b3898c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ed95f8f1607c1adb4b4",
+    "url": "/static/css/2.7a90db8a.chunk.css"
+  },
+  {
+    "revision": "04988d7aa672c3f46e52",
+    "url": "/static/css/main.0bcf8cab.chunk.css"
+  },
+  {
+    "revision": "4ed95f8f1607c1adb4b4",
+    "url": "/static/js/2.c81d768f.chunk.js"
+  },
+  {
+    "revision": "05993105e4d9c7eede64bcd6e4db7cd5",
+    "url": "/static/js/2.c81d768f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04988d7aa672c3f46e52",
+    "url": "/static/js/main.092f22da.chunk.js"
+  },
+  {
+    "revision": "d080715c058dfedfe9ba",
+    "url": "/static/js/runtime-main.263c3f02.js"
+  }
+]);
