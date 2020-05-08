92e1b6cdac6245d12dbcea4086d0b93681c8c16f
leio99 <amicoexe@gmail.com>
2020-05-08 11:52:45 +0200

    CSV Ore di stage

--- FrontEnd/src/comp/admin/StudentStage.tsx
+++ FrontEnd/src/comp/admin/StudentStage.tsx
@@ -3,9 +3,11 @@ import Axios from "axios"
 import { Modal, Spin, Icon, Tooltip, Button } from "antd"
 import { IStage } from "../../models/IStage"
 import { siteUrl, formatItalian, convertFromUTC } from "../../utilities"
+import { CSVLink } from "react-csv"
+import { IStudent } from "../../models/IStudent"
 
 export interface IProps{
-    readonly idStudente: number
+    readonly studente: IStudent
     readonly toggleModal: () => void
     readonly show: boolean
 }
@@ -23,8 +25,15 @@ export default class StudentStage extends React.PureComponent<IProps, IState>{
     }
 
     componentDidMount = () => {
-        Axios.get(siteUrl+"/api/studenti/getorestage/"+this.props.idStudente).then(response => {
-            let oreList = response.data as IStage[]
+        Axios.get(siteUrl+"/api/studenti/getorestage/"+this.props.studente.idStudente).then(response => {
+            let data = response.data,
+            oreList = data.map((o: any) => {
+                o.data = formatItalian(o.data)
+                o.oraInizio = convertFromUTC(o.oraInizio + "Z")
+                o.oraFine = convertFromUTC(o.oraFine + "Z")
+
+                return o as IStage
+            })
 
             this.setState({
                 oreList
@@ -34,11 +43,16 @@ export default class StudentStage extends React.PureComponent<IProps, IState>{
 
     render = () => {
         const { oreList } = this.state,
-        { toggleModal, show } = this.props,
+        { toggleModal, show, studente } = this.props,
         icon = <Icon type="loading" style={{ fontSize: 50 }} spin />
 
         return <Modal visible={show} title="Ore di stage svolte" onCancel={toggleModal} width="60%" footer={[
-            <Button onClick={toggleModal} type="primary">Chiudi</Button>
+            <Button type="primary" disabled={!oreList || oreList.length === 0}>
+                <CSVLink data={oreList ? oreList.map(o => Object.values(o)) : null} filename={"oreStage"+studente.nome+studente.cognome+".csv"}>
+                    <i className="far fa-arrow-to-bottom mr-2"></i> Scarica CSV
+                </CSVLink>
+            </Button>,
+            <Button onClick={toggleModal}>Chiudi</Button>
         ]}>
             {
                 !oreList ? <div className="text-center">
@@ -58,12 +72,12 @@ export default class StudentStage extends React.PureComponent<IProps, IState>{
                                 {
                                     oreList.map(s => {        
                                         return <tr>
-                                            <td>{formatItalian(s.data)}</td>
+                                            <td>{s.data}</td>
                                             <Tooltip title={s.argomento}>
                                                 <td style={{maxWidth: 0}} className="text-truncate">{s.argomento}</td>
                                             </Tooltip>
-                                            <td style={{maxWidth: 0}} className="text-truncate">{convertFromUTC(s.oraInizio + "Z")}</td>
-                                            <td style={{maxWidth: 0}} className="text-truncate">{convertFromUTC(s.oraFine + "Z")}</td>
+                                            <td style={{maxWidth: 0}} className="text-truncate">{s.oraInizio}</td>
+                                            <td style={{maxWidth: 0}} className="text-truncate">{s.oraFine}</td>
                                             <td style={{maxWidth: 0}} className="text-truncate">{s.totaleRelativo}</td>
                                         </tr>
                                     })
